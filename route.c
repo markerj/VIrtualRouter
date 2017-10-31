3ecea8bd1c0d2005f68a246057351ef202cc04f1
markerj <markerj@mail.gvsu.edu>
2017-10-30 23:01:57 -0400

    Update route.c

--- route.c
+++ route.c
@@ -93,9 +93,19 @@ int main(){
     printf("Destination address: %s\n", tempEth);
     tempEth=ether_ntoa((struct ether_addr*) &eth->ether_shost);
     printf("Source address: %s\n", tempEth);	
-	  
+    int size = sizeof(eth->ether_dhost)+sizeof(eth->ether_shost)+sizeof(eth->ether_type);
+    printf("Size of Eth header: %d\n", size); 
+	 
     struct arpheader {
-
+        unsigned short int   arp_hardware;		/* Format of hardware address.  */
+    	unsigned short int   arp_protocol;		/* Format of protocol address.  */
+    	unsigned char        arp_hardware_length;	/* Length of hardware address.  */
+    	unsigned char        arp_protocol_length;	/* Length of protocol address.  */
+    	unsigned short int   arp_op;			/* ARP opcode (command).  */
+    	unsigned char        arp_sha[6];		/* Sender hardware address.  */
+    	unsigned char        arp_sip[4];		/* Sender IP address.  */
+    	unsigned char        arp_tha[6];		/* Target hardware address.  */
+    	unsigned char        arp_tip[4];		/* Target IP address.  */
     }
     	  
     struct arpheader arphdr;
