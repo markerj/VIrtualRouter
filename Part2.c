487cd41ee59aefd2e744a1c6673f9cb9331f26e4
Tyler <paquett@mail.gvsu.edu>
2017-11-07 16:16:36 -0500

    testing

--- Part2.c
+++ Part2.c
@@ -434,7 +434,7 @@ void *interfaces(void *args)
                 {
                     printf("Received ARP response\n");
                     unsigned char destaddress[6];
-                    memcpy(arphdr->src_addr, destaddress);
+                    memcpy(arphdr->src_addr, destaddress, 6);
                     printf("Destination Address is: %s\n", destaddress);
                 }
 
