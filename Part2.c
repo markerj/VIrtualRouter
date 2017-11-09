66a39a451e1b09f66e6de83c7010de0809b98b4a
Tyler <paquett@mail.gvsu.edu>
2017-11-08 23:23:06 -0500

    testing

--- Part2.c
+++ Part2.c
@@ -432,7 +432,7 @@ void *interfaces(void *args)
 
                 //Received arp response, forward packet to corresponding mac address
                 //For Part2 we just need to print mac address obtained from arp
-                /*else if(ntohs(arphdr->op) == 2)
+                else if(ntohs(arphdr->op) == 2)
                 {
                     printf("From eth%d thread: Received ARP response\n", ethNum);
                     printf("From eth%d thread: Destination Address: %02x:%02x:%02x:%02x:%02x:%02x\n",
@@ -443,7 +443,7 @@ void *interfaces(void *args)
                            ethhdr->eth_src[3],
                            ethhdr->eth_src[4],
                            ethhdr->eth_src[5]);
-                }*/
+                }
 
             }
 
